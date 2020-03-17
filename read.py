e2404a4d51e731c9fc6071af8bda168c04fca44a
Bruce <bruce79848@gmail.com>
2020-03-17 18:17:18 +0800

    Upload a final version

--- read.py
+++ read.py
@@ -3,9 +3,12 @@ count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
-		count += 1 #快寫法，等於count = count + 1
-		if count % 100000 == 0:
+		count += 1
+		if count % 10000 == 0:
 			print(len(data))
-print(data[0])
-print('--------------------------------')
-print(data[1])
+print('Finish loading, there are totally', len(data), 'sets of data.')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('And the average length of the comments is', sum_len / len(data))
