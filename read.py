e9077d178ee408a058ec081485317e529a32f962
Bruce <bruce79848@gmail.com>
2020-03-19 17:55:30 +0800

    Update a new version with filter.

--- read.py
+++ read.py
@@ -11,4 +11,10 @@ print('Finish loading, there are totally', len(data), 'sets of data.')
 sum_len = 0
 for d in data:
 	sum_len += len(d)
-print('The average length of each comment is', sum_len / len(data), 'word.')
+print('The average length of each comment is', sum_len / len(data), 'word.')\
+
+new = []
+for d in data:
+	if len(d) <= 100:
+		new.append(d)
+print('There are totally', len(new), 'comments which have less than 100 words.')
